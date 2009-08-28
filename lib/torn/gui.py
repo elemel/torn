31056aea6b8a95a4c0564d1b0ff36427b9668d08
Mikael Lind <elemel@elemel.se>
2009-08-28 08:07:03 +0200

    extracted module geometry

--- lib/torn/gui.py
+++ lib/torn/gui.py
@@ -7,6 +7,7 @@ from math import *
 import pyglet
 from pyglet.gl import *
 import sys
+from torn.geometry import *
 
 def draw_polygon(vertices, closed=True):
     vertices = list(vertices)
@@ -122,67 +123,6 @@ class CameraController(object):
 
 class Model(object):
     pass
-
-class Polygon(object):
-    def __init__(self, vertices, closed=True):
-        self.vertices = list(v.copy() for v in vertices)
-        assert len(self.vertices) >= 1
-        assert all(isinstance(v, Point2) for v in self.vertices)
-        assert type(closed) is bool
-        self.closed = closed
-
-    def copy(self):
-        return Polygon(self.vertices, self.closed)
-
-    @property
-    def edges(self):
-        edges = [LineSegment2(self.vertices[i], self.vertices[i + 1])
-                 for i in xrange(len(self.vertices) - 1)]
-        if self.closed and len(self.vertices) >= 3:
-            edges.append(LineSegment2(self.vertices[-1], self.vertices[0]))
-        return edges
-
-    @property
-    def lengths(self):
-        return [e.length for e in self.edges]
-
-    @property
-    def starting_point(self):
-        return self.vertices[0]
-
-    @property
-    def end_point(self):
-        return self.vertices[0 if self.closed else -1]
-
-    @property
-    def max_radius(self):
-        return 0 if self.closed else sum(self.lengths)
-
-    @property
-    def min_radius(self):
-        if self.closed or len(self.vertices) <= 1:
-            return 0
-        lengths = [e.length for e in self.edges]
-        lengths.sort()
-        return max(0, lengths[-1] - sum(lengths[:-1]))
-
-    @property
-    def area(self):
-        """
-        http://local.wasp.uwa.edu.au/~pbourke/geometry/clockwise/
-        """
-        if not self.closed:
-            return 0
-        return sum(v1.x * v2.y - v2.x * v1.y
-                   for v1, v2 in izip(self.vertices,
-                                      self.vertices[1:] + self.vertices[:1]))
-
-    @property
-    def clockwise(self):
-        return self.area < 0
-
-    def reverse(self):
-        self.vertices.reverse()
 
 class Level(Model):
     def __init__(self):
--- lib/torn/main.py
+++ lib/torn/main.py
@@ -11,6 +11,7 @@ from pyglet.gl import *
 import rabbyt
 import random
 import sys
+from torn.geometry import *
 from torn import ik
 
 def rad_to_deg(angle_rad):
@@ -78,49 +79,6 @@ def load_object(path):
 def save_object(obj, path):
     file_ = open(path, 'wb')
     pickle.dump(obj, file_, pickle.HIGHEST_PROTOCOL)
-
-class Polygon(object):
-    def __init__(self, vertices, closed=True):
-        self.vertices = list(v.copy() for v in vertices)
-        assert len(self.vertices) >= 1
-        assert all(isinstance(v, Point2) for v in self.vertices)
-        assert type(closed) is bool
-        self.closed = closed
-
-    def copy(self):
-        return Polygon(self.vertices, self.closed)
-
-    @property
-    def edges(self):
-        edges = [LineSegment2(self.vertices[i], self.vertices[i + 1])
-                 for i in xrange(len(self.vertices) - 1)]
-        if self.closed:
-            edges.append(LineSegment2(self.vertices[-1], self.vertices[0]))
-        return edges
-
-    @property
-    def lengths(self):
-        return [e.length for e in self.edges]
-
-    @property
-    def starting_point(self):
-        return self.vertices[0]
-
-    @property
-    def end_point(self):
-        return self.vertices[0 if self.closed else -1]
-
-    @property
-    def max_radius(self):
-        return 0 if self.closed else sum(self.lengths)
-
-    @property
-    def min_radius(self):
-        if self.closed or len(self.vertices) <= 1:
-            return 0
-        lengths = [e.length for e in self.edges]
-        lengths.sort()
-        return max(0, lengths[-1] - sum(lengths[:-1]))
 
 class Skeleton(object):
     def __init__(self):
